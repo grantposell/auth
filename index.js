9473cef76819dbb4601d707f12de2dfa406fb48b
grantposell <grantposell@gmail.com>
2019-05-25 14:23:25 -0500

    Finished authentication

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
 //import Routes
 const authRoute = require('./routes/auth')
-
+const postRoute = require('./routes/posts')
 dotenv.config();
 
 //Connect to DB
@@ -16,9 +15,9 @@ mongoose.connect(process.env.DB_Connect,{ useNewUrlParser: true },
 //Middleware
 app.use(express.json());
 
-
 //Route Middleware
 app.use('/api/user', authRoute);
+app.use('/api/posts', postRoute);
 
 
 app.listen(3000, () => console.log('Server is up and running'));
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs');
-const {registerValidation} = require('../validation')
+const {registerValidation, loginValidation} = require('../validation')
 
 
 router.post('/register', async (req, res) => {
     const { error } = registerValidation(req.body)
-    if(error) res.status(400).send(error.details[0].message);
+    if(error) return res.status(400).send(error.details[0].message);
 
     //Checking if user is in database
     const emailExist = await User.findOne({email: req.body.email});
@@ -22,16 +23,32 @@ router.post('/register', async (req, res) => {
         email: req.body.email,
         password: hashedPassword
     });
-    try{
+    try {
         const savedUser= await user.save()
         res.send({user: user._id});
-    }catch(err){
+    } catch(err){
         res.status(400).send(err);
     }
 });
 
 //Login
-router.post('/login', )
+router.post('/login', async (req, res) => {
+    const { error } = loginValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    //Checking if the email exists
+    const user = await User.findOne({email: req.body.email});
+    if(!user) return res.status(400).send('Email or Password is wrong');
+    //Password is correct
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if(!validPass) return res.status(400).send('Invalid Password');
+
+    //Create and assign a token
+    const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+    res.header('auth-token', token).send(token);
+
+    res.send('Logged In!');
+});
 
 
 
