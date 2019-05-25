1d0f732ae51893fd924a973f194c863b17181f4a
grantposell <grantposell@gmail.com>
2019-05-24 21:58:44 -0500

    Added salt and password noncaching

--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,37 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const bcrypt = require('bcryptjs');
+const {registerValidation} = require('../validation')
 
 
 router.post('/register', async (req, res) => {
-    const {error} = Joi.validate(req.body, schema);
+    const { error } = registerValidation(req.body)
     if(error) res.status(400).send(error.details[0].message);
-    
+
+    //Checking if user is in database
+    const emailExist = await User.findOne({email: req.body.email});
+    if(emailExist) return res.status(400).send('Email already exists');
+
+    //Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    //Create User
     const user = new User({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
     try{
         const savedUser= await user.save()
-        res.send(savedUser);
+        res.send({user: user._id});
     }catch(err){
         res.status(400).send(err);
     }
 });
+
+//Login
+router.post('/login', )
 
 
 
--- validation.js
+++ validation.js
@@ -3,7 +3,7 @@ const Joi = require('@hapi/joi');
 
 
 //Register Validation
-const registerValidation = (data) => {
+const registerValidation = data => {
     const schema = {
         firstName: Joi.string().min(3).required(),
         lastName: Joi.string().min(2).required(),
@@ -13,13 +13,12 @@ const registerValidation = (data) => {
     return Joi.validate(data,schema);
 };
 //Register Validation
-const loginValidation = (data) => {
+const loginValidation = data => {
     const schema = {
-        firstName: Joi.string().min(3).required(),
-        lastName: Joi.string().min(2).required(),
         email: Joi.string().email({ minDomainSegments: 2 }).required(),
         password: Joi.string().min(6).required()
     };
     return Joi.validate(data,schema);
 };
 module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation
